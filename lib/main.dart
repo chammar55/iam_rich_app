4d16f270c669d4ab2579c6b832e8ff1d460f6a05
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2022-12-12 19:03:59 +0500

    rich

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import"package:flutter/material.dart";
+
+void main()=> runApp(MaterialApp (home: Scaffold( appBar: AppBar(title: Center(child: Text('Hello World'),
+),backgroundColor: Colors.lightGreen,),backgroundColor: Colors.blueGrey,body: Center(child: Image(image: AssetImage('diamond.png'),),),),),);
+
